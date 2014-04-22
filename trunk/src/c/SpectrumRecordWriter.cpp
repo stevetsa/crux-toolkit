9e1db5e0de4ef21fae14e5642c053da2c1b67b6e
acheng94 <acheng94@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2014-04-22 22:27:17 +0000

    Alice's First Checkout

---
+++ trunk/src/c/SpectrumRecordWriter.cpp
@@ -0,0 +1,325 @@
+#include <cmath>
+#include "pwiz/data/msdata/MSDataFile.hpp"
+#include "pwiz/data/msdata/SpectrumInfo.hpp"
+#include "tide/records.h"
+#include "tide/mass_constants.h"
+
+#include "Peak.h"
+#include "SpectrumRecordWriter.h"
+#include "carp.h"
+#include "crux-utils.h"
+
+// For printing uint64_t values
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+int SpectrumRecordWriter::scanCounter_ = 0;
+int SpectrumRecordWriter::removePrecursorPeak_ = 0;
+FLOAT_T SpectrumRecordWriter::removePrecursorTolerance_ = 0;
+
+/**
+ * Converts a spectra file to spectrumrecords format for use with tide-search.
+ * Spectra file is read by pwiz. Returns true on successful conversion.
+ */
+bool SpectrumRecordWriter::convert(
+  const string& infile, ///< spectra file to convert
+  string outfile  ///< spectrumrecords file to output
+) {
+  // Check options to remove peaks around precursor
+  removePrecursorPeak_ = get_boolean_parameter("remove-precursor-peak");
+  removePrecursorTolerance_ = get_double_parameter("remove-precursor-tolerance");
+  /*if (removePrecursorPeak_ < 0 || removePrecursorPeak_ > 2) {
+    carp(CARP_FATAL, "remove_precursor_peak must be 0, 1, or 2.");
+  }*/
+
+  // Open infile
+  pwiz::msdata::MSDataFile* msd;
+  try {
+    msd = new pwiz::msdata::MSDataFile(infile);
+  } catch (...) {
+    return false;
+  }
+
+  // Write outfile
+  pb::Header header;
+  header.set_file_type(pb::Header::SPECTRA);
+
+  const vector<pwiz::msdata::SourceFilePtr>& sourceFilePtrs =
+    msd->fileDescription.sourceFilePtrs;
+  for (vector<pwiz::msdata::SourceFilePtr>::const_iterator s = sourceFilePtrs.begin();
+       s != sourceFilePtrs.end();
+       ++s) {
+    pb::Header_Source* source = header.add_source();
+    source->set_filename((*s)->location + "/" + (*s)->name);
+    size_t pos = (*s)->name.rfind('.');
+    string extension = (pos == string::npos) ? "UNKNOWN" : (*s)->name.substr(pos + 1);
+    source->set_filetype(extension);
+  }
+
+  header.mutable_spectra_header()->set_sorted(false);
+
+  HeadedRecordWriter writer(outfile, header);
+  if (!writer.OK()) {
+    return false;
+  }
+
+  scanCounter_ = 0;
+
+  // Go through the spectrum list and write each spectrum
+  pwiz::msdata::SpectrumList& sl = *(msd->run.spectrumListPtr);
+  for (size_t i = 0; i < sl.size(); ++i) {
+    pwiz::msdata::SpectrumPtr s;
+    try {
+      s = sl.spectrum(i, true);
+    } catch (...) {
+      carp(CARP_FATAL, "Could not parse %s. Error occurred on spectrum %d "
+                       "of the spectrum list.", infile.c_str(), i);
+    }
+
+    pb::Spectrum pb_spectrum = getPbSpectrum(s);
+    if (pb_spectrum.charge_state_size() > 0) {
+      writer.Write(&pb_spectrum);
+    }
+  }
+
+  delete msd;
+  return true;
+}
+
+/**
+ * Return a pb::Spectrum from a pwiz SpectrumPtr
+ * If spectrum is ms1, or has no precursors/peaks then return empty pb::Spectrum
+ */
+pb::Spectrum SpectrumRecordWriter::getPbSpectrum(
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  if (s->cvParam(pwiz::cv::MS_ms_level).valueAs<int>() == 1 ||
+      s->precursors.empty() || s->precursors[0].selectedIons.empty() ||
+      s->defaultArrayLength == 0) {
+    return pb::Spectrum();
+  }
+
+  pb::Spectrum pb_spectrum;
+
+  setScanNumber(&pb_spectrum, s);
+  setPrecursorMz(&pb_spectrum, s);
+  addChargeStates(&pb_spectrum, s);
+  addPeaks(&pb_spectrum, s);
+
+  return pb_spectrum;
+}
+
+/**
+ * Set scan number for a pb::Spectrum
+ */
+void SpectrumRecordWriter::setScanNumber(
+  pb::Spectrum* spectrum,
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  string scan_num = pwiz::msdata::id::translateNativeIDToScanNumber(
+    pwiz::cv::MS_scan_number_only_nativeID_format, s->id);
+  if (scanCounter_ > 0 || scan_num.empty()) {
+    carp_once(CARP_INFO, "Parser could not determine scan numbers for this "
+                         "file, using ordinal numbers as scan numbers.");
+    spectrum->set_spectrum_number(++scanCounter_);
+  } else {
+    spectrum->set_spectrum_number(atoi(scan_num.c_str()));
+  }
+}
+
+/**
+ * Set precursor m/z for a pb::Spectrum
+ */
+void SpectrumRecordWriter::setPrecursorMz(
+  pb::Spectrum* spectrum,
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  pwiz::msdata::SelectedIon& si = s->precursors[0].selectedIons[0];
+  double mz = si.cvParam(pwiz::cv::MS_selected_ion_m_z).valueAs<double>();
+  spectrum->set_precursor_m_z(mz);
+}
+
+/**
+ * Add charge state information to a pb::Spectrum
+ */
+void SpectrumRecordWriter::addChargeStates(
+  pb::Spectrum* spectrum,
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  pwiz::msdata::SelectedIon& si = s->precursors[0].selectedIons[0];
+  pwiz::msdata::CVParam chargeParam = si.cvParam(pwiz::cv::MS_charge_state);
+  if (!chargeParam.empty()) {
+    spectrum->mutable_charge_state()->Add(chargeParam.valueAs<int>());
+  } else {
+    for (vector<pwiz::msdata::CVParam>::const_iterator param = si.cvParams.begin();
+         param != si.cvParams.end();
+         ++param) {
+      if (param->cvid == pwiz::cv::MS_possible_charge_state) {
+        spectrum->mutable_charge_state()->Add(param->valueAs<int>());
+      }
+    }
+    if (spectrum->charge_state_size() == 0) {
+      // Need to calculate charge states
+      addCalculatedChargeStates(spectrum, s);
+    }
+  }
+}
+
+/**
+ * Add calculated charge state information to a pb::Spectrum
+ */
+void SpectrumRecordWriter::addCalculatedChargeStates(
+  pb::Spectrum* spectrum,
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  sortSpectrumPtrPeaks(s);
+  const pwiz::msdata::BinaryDataArray& mzs = *(s->getMZArray());
+  const pwiz::msdata::BinaryDataArray& intensities = *(s->getIntensityArray());
+  vector<Peak*> peaks;
+  for (size_t i = 0; i < s->defaultArrayLength; ++i) {
+    peaks.push_back(new Peak(intensities.data[i], mzs.data[i]));
+  }
+  switch (choose_charge(spectrum->precursor_m_z(), peaks)) {
+  case SINGLE_CHARGE_STATE:
+    spectrum->mutable_charge_state()->Add(1);
+    break;
+  case MULTIPLE_CHARGE_STATE:
+    spectrum->mutable_charge_state()->Add(2);
+    spectrum->mutable_charge_state()->Add(3);
+    break;
+  default:
+    carp(CARP_FATAL, "Could not determine charge state for scan %d",
+         spectrum->spectrum_number());
+  }
+  for (vector<Peak*>::iterator i = peaks.begin(); i != peaks.end(); ++i) {
+    delete *i;
+  }
+}
+
+/**
+ * Add peaks to a pb::Spectrum
+ */
+void SpectrumRecordWriter::addPeaks(
+  pb::Spectrum* spectrum,
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  // Get each m/z, intensity pair
+  const pwiz::msdata::BinaryDataArray& mzs = *(s->getMZArray());
+  const pwiz::msdata::BinaryDataArray& intensities = *(s->getIntensityArray());
+  int mz_denom = getDenom(mzs.data);
+  int intensity_denom = getDenom(intensities.data);
+  spectrum->set_peak_m_z_denominator(mz_denom);
+  spectrum->set_peak_intensity_denominator(intensity_denom);
+  uint64_t last = 0;
+  int last_index = -1;
+  uint64_t intensity_sum = 0;
+
+  for (size_t i = 0; i < s->defaultArrayLength; ++i) {
+    const double& peakMz = mzs.data[i];
+    if (removePrecursorPeak(*spectrum, peakMz)) {
+      continue;
+    }
+    uint64_t mz = peakMz * mz_denom + 0.5;
+    uint64_t intensity = intensities.data[i] * intensity_denom + 0.5;
+    if (mz < last) {
+      // Unsorted peaks, sort and restart loop
+      sortSpectrumPtrPeaks(s);
+
+      spectrum->clear_peak_m_z();
+      spectrum->clear_peak_intensity();
+      last = 0;
+      last_index = -1;
+      intensity_sum = 0;
+      i = -1;
+      continue;
+    } else if (mz == last) {
+      intensity_sum += intensity;
+      spectrum->set_peak_intensity(last_index, intensity_sum);
+    } else {
+      spectrum->add_peak_m_z(mz - last);
+      spectrum->add_peak_intensity(intensity);
+      last = mz;
+      intensity_sum = intensity;
+      ++last_index;
+    }
+  }
+}
+
+/**
+ * Check if this peak should be excluded
+ * Charge states must be set for pb_spectrum
+ */
+bool SpectrumRecordWriter::removePrecursorPeak(
+  const pb::Spectrum& pb_spectrum,
+  double peakMz
+) {
+  switch (removePrecursorPeak_) {
+  case 1:
+    return fabs(pb_spectrum.precursor_m_z() - peakMz) <= removePrecursorTolerance_;
+  /*case 2: {
+    // all charge reduced precursor peaks
+    for (int i = 0; i < pb_spectrum.charge_state_size(); ++i) {
+      int charge = pb_spectrum.charge_state(i);
+      double mass = pb_spectrum.precursor_m_z() * charge -
+        (charge - 1) - MassConstants::proton;
+      for (int j = 1; j < charge; ++j) {
+        double mz = (mass + (j - 1) * MassConstants::proton) / j;
+        if (fabs(mz - peakMz) <= removePrecursorTolerance_) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }*/
+  default:
+    return false;
+  }
+}
+
+/**
+ * Sort peaks in a pwiz SpectrumPtr
+ */
+void SpectrumRecordWriter::sortSpectrumPtrPeaks(
+  const pwiz::msdata::SpectrumPtr& s
+) {
+  vector<pwiz::msdata::MZIntensityPair> sortedPeaks;
+  s->getMZIntensityPairs(sortedPeaks);
+  sort(sortedPeaks.begin(), sortedPeaks.end(), comparePeaks);
+  s->setMZIntensityPairs(sortedPeaks, pwiz::cv::MS_number_of_detector_counts);
+}
+
+/**
+ * See whether all vals can be accomodated by denom when rendered as a fraction.
+ */
+bool SpectrumRecordWriter::checkDenom(
+  const vector<double>& vals, ///< values to check
+  int denom ///< denominator to check against
+) {
+  double d_denom = denom;
+  for (int i = 0; i < vals.size(); ++i) {
+    double x = vals[i] * d_denom;
+    if (fabs(x - google::protobuf::uint64(x+0.5)) >= 0.001)
+      return false;
+  }
+  return true;
+}
+
+/**
+ * See how much precision is given in the vals array.
+ */
+int SpectrumRecordWriter::getDenom(
+  const vector<double>& vals  ///< values to check
+) {
+  const int kMaxPrecision = 10000; // store at most 4 digits of precision
+  for (int precision = 1; precision < kMaxPrecision; precision *= 10)
+    if (checkDenom(vals, precision))
+      return precision;
+  return kMaxPrecision;
+}
+
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 2
+ * End:
+ */
---
+++ trunk/src/c/test/performance-tests/run-performance-test.sh
@@ -0,0 +1,234 @@
+#!/bin/bash -efx
+# AUTHOR: William Stafford Noble
+
+# This script runs a performance test on the crux toolkit.  Its usage
+# and output are described in ./performance.html.  The MS2 file comes
+# from the PAnDA paper by Hoopman et al. (JPR 2009).  Extensive
+# documentation of this particular data set is available from
+# crux-projects/panda-data.
+
+# The location of the crux binary.
+CRUX=../../crux
+
+# Increase the file limit for Crux. (Necessary on MacOS.)
+ulimit -n 1024
+
+# Initialize the gnuplot script.
+gnuplot=performance.gnuplot
+echo set output \"/dev/null\" > $gnuplot
+echo set terminal png >> $gnuplot
+echo set xlabel \"q-value threshold\" >> $gnuplot
+echo set ylabel \"Number of accepted PSMs\" >> $gnuplot
+echo set xrange \[0:0.1\] >> $gnuplot
+echo set key center right >> $gnuplot
+# Insert dummy plot so we can use "replot" consistently below.
+echo plot 0 notitle with dots >> $gnuplot 
+
+# Create the parameter file.
+parameters=crux.param
+
+# Enzymatic digestion rules.
+echo enzyme=trypsin >> $parameters
+echo search_enzyme_number=1 >> $parameters 
+echo digestion=full-digest > $parameters
+echo num_enzyme_termini=2 >> $parameters
+echo missed-cleavages=0 >> $parameters
+echo allowed_missed_cleavage=0 >> $parameters
+
+# Precursor selection rules.
+echo precursor-window=3 >> $parameters
+echo precursor-window-type=mass >> $parameters
+echo peptide_mass_tolerance=3 >> $parameters
+echo peptide_mass_units=0 >> $parameters # 0=amu, 1=mmu, 2=ppm
+echo precursor_tolerance_type=0 >> $parameters # 0=MH+ (default), 1=precursor m/z
+
+# Precursor mass type.
+echo isotopic-mass=mono >> $parameters
+echo monoisotopic-precursor=T >> $parameters
+echo mass_type_parent=1 >> $parameters # 1=monoisotopic
+
+# Fragment mass type.  Tides uses only monoisotopic.
+echo fragment-mass=mono >> $parameters
+echo mass_type_fragment=1 >> $parameters # 1=monoisotopic
+
+# Decoys.
+echo decoy-format=peptide-reverse >> $parameters
+echo num-decoys-per-target=1 >> $parameters
+echo decoy_search=2 >> $parameters  # 2 = separate decoy search
+
+# Report the top 5 matches.
+echo num_results=6 >> $parameters
+echo num_output_lines=5 >> $parameters
+echo top-match=5 >> $parameters
+
+# Precursor removal.
+echo remove_precursor_peak=1 >> $parameters
+echo remove_precursor_tolerance=15 >> $parameters
+echo remove-precursor-peak=T >> $parameters
+echo remove-precursor-tolerance=15 >> $parameters
+
+# Flanking peaks.
+echo use-flanking-peaks=F >> $parameters
+echo theoretical_fragment_ions=1 >> $parameters # 0 = flanks; 1 = no flanks
+
+# Fragment m/z discretization.  This is fixed in Tide.
+echo fragment_bin_offset=0.5 >> $parameters
+echo fragment_bin_tol=1.000508 >> $parameters
+
+# Other Crux parameters.
+echo compute-sp=T >> $parameters
+echo verbosity=40 >> $parameters
+echo overwrite=T >> $parameters
+echo peptide-list=T >> $parameters
+
+# Comet parameters
+echo add_C_cysteine=57.021464 >> $parameters
+echo num_threads=1 >> $parameters # Multithreaded sometimes dumps core.
+echo digest_mass_range=200 7200 >> $parameters
+echo max_fragment_charge=2 >> $parameters
+echo isotope_error=0 >> $parameters
+echo use_A_ions=0 >> $parameters
+echo use_B_ions=1 >> $parameters
+echo use_C_ions=0 >> $parameters
+echo use_X_ions=0 >> $parameters
+echo use_Y_ions=1 >> $parameters
+echo use_Z_ions=0 >> $parameters
+echo use_NL_ions=1 >> $parameters
+echo variable_mod1=0.0 X 0 3 >> $parameters
+echo variable_mod2=0.0 X 0 3 >> $parameters
+echo "[COMET_ENZYME_INFO]" >> $parameters
+echo "0.  No_enzyme              0      -           -" >> $parameters
+echo "1.  Trypsin                1      KR          P" >> $parameters
+
+# Create the index.
+db=worm+contaminants
+fasta=$db.fa
+if [[ -e $db ]]; then
+  echo Skipping create-index.
+else
+  $CRUX tide-index --output-dir tide-index --parameter-file $parameters \
+     $fasta $db
+fi
+
+ms2=051708-worm-ASMS-10.ms2
+
+# Do the whole test twice, once for each search tool.
+for searchtool in comet tide-search; do
+
+  # Do we use an index or the fasta?
+  if [[ $searchtool == "comet" ]]; then
+    proteins=$fasta
+  else
+    proteins=$db
+  fi
+
+  # Run the search.
+  if [[ -e $searchtool/$searchtool.target.txt ]]; then
+    echo Skipping search-for-matches.
+  else  
+    $CRUX $searchtool \
+      --parameter-file crux.param --output-dir $searchtool \
+      $ms2 $proteins
+  fi
+
+  # Run calibrate-scores
+  if [[ -e $searchtool/qvalues.target.txt ]]; then
+    echo Skipping crux calibrate-scores.
+  else
+    $CRUX calibrate-scores \
+      --output-dir $searchtool \
+      $searchtool/$searchtool.target.txt
+  fi
+
+  $CRUX sort-by-column --column-type real --ascending true $searchtool/qvalues.target.txt "decoy q-value (xcorr)" | $CRUX extract-columns - "decoy q-value (xcorr)" > $searchtool/qvalues.xcorr.txt
+  echo replot \"$searchtool/qvalues.xcorr.txt\" using 1:0 title \"$searchtool xcorr\" with lines >> $gnuplot
+
+  if [[ $searchtool == "comet" ]]; then
+    $CRUX extract-columns $searchtool/qvalues.target.txt "decoy q-value (e-value)" > $searchtool/qvalues.evalue.txt 
+    echo replot \"$searchtool/qvalues.evalue.txt\" using 1:0 title \"$searchtool e-value\" with lines >> $gnuplot
+  fi
+
+  # Run Crux percolator
+  if [[ -e $searchtool/percolator.target.psms.txt ]]; then
+    echo Skipping crux percolator.
+  else
+    $CRUX percolator \
+      --output-dir $searchtool \
+      --feature-file T \
+      $searchtool/$searchtool.target.txt
+  fi
+
+  $CRUX extract-columns $searchtool/percolator.target.psms.txt "percolator q-value" > $searchtool/qvalues.percolator.txt
+  echo replot \"$searchtool/qvalues.percolator.txt\" using 1:0 title \"$searchtool percolator\" with lines >> $gnuplot
+
+  # Run q-ranker.
+  if [[ -e $searchtool/q-ranker.target.psms.txt ]]; then
+    echo Skipping q-ranker.
+  else
+    $CRUX q-ranker \
+      --output-dir $searchtool \
+      --feature-file T \
+      --separate-searches $searchtool/$searchtool.decoy.txt \
+      $ms2 $searchtool/$searchtool.target.txt
+  fi
+
+  $CRUX extract-columns $searchtool/q-ranker.target.psms.txt "q-ranker q-value" > $searchtool/qvalues.qranker.txt
+  echo replot \"$searchtool/qvalues.qranker.txt\" using 1:0 title \"$searchtool q-ranker\" with lines >> $gnuplot
+
+  # Run Barista.
+  # if [[ -e $searchtool/barista.target.psms.txt ]]; then
+  #   echo Skipping barista.
+  # else
+  #   $CRUX barista \
+  #     --output-dir $searchtool \
+  #     --feature-file T \
+  #     --overwrite T \
+  #     --separate-searches $searchtool/$searchtool.decoy.txt \
+  #     $fasta $ms2 $searchtool/$searchtool.target.txt
+  # fi
+
+  # $CRUX extract-columns $searchtool/barista.target.psms.txt "barista q-value" > $searchtool/qvalues.barista.txt
+  # echo replot \"$searchtool/qvalues.barista.txt\" using 1:0 title \"$searchtool barista\" with lines >> $gnuplot
+  
+done
+
+# Finalize the gnuplot script.
+echo set output >> $gnuplot
+echo replot >> $gnuplot
+
+# Make the plot.
+gnuplot $gnuplot > performance.png
+
+# Extract a subset of columns for use in comparing XCorr scores.
+for searchtool in tide-search comet; do
+  searchFile=$searchtool/$searchtool.target.txt
+  reducedFile=$searchtool/$searchtool.target.reduced.txt
+  $CRUX extract-columns $searchFile \
+     "scan,charge,sequence,xcorr score" \
+     | awk 'NR > 1' \
+     | awk '{print $1 "~" $2 "~" $3 "\t" $4}' \
+     | sort -k 1b,1 \
+     > $reducedFile
+done
+
+# Join the two sets of scores.
+echo -e "scan\tcharge\tpeptide\ttide-search xcorr\tcomet xcorr" > xcorr.txt
+join \
+    tide-search/tide-search.target.reduced.txt \
+    comet/comet.target.reduced.txt \
+  | awk -F "~" '{print $1 " " $2 " " $3}' \
+  | awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5}' \
+  | sort -n \
+  >> xcorr.txt
+
+# Create a scatter plot of XCorr scores.
+gnuplot=xcorr.gnuplot
+echo set output \"/dev/null\" > $gnuplot
+echo set terminal png >> $gnuplot
+echo set xlabel \"Tide XCorr\" >> $gnuplot
+echo set ylabel \"Comet XCorr\" >> $gnuplot
+echo plot x notitle with lines >> $gnuplot
+echo replot \"xcorr.txt\" using 4\:5 notitle >> $gnuplot
+echo set output >> $gnuplot
+echo replot >> $gnuplot
+gnuplot $gnuplot > xcorr.png
